170c1f9ad59a450e45cd99380387d54092891e61
azaxsc <guilhermecruz2014co2@gmail.com>
2022-05-02 10:22:16 -0300

    Execução adicionada como método e outras melhorias.

--- classe.py
+++ classe.py
@@ -18,31 +18,77 @@ class posfix:
 
     def convert(self):
         flag = False
-        
-        for char in self.input:
+        try:
+            for char in self.input:
+                
+                if ((char.isascii() and char not in ['+', '.', '*', '(', ')', '\\', ' ']) or flag): 
+                    if flag:
+                        flag = False
+                        self.output.append('\\'+char)
+                    else:    
+                        self.output.append(char)
+                elif (char == '\\'):
+                    flag = True
+
+                elif char == '(':
+                    self.operatorsStack.push(char)
+                elif char == ')':
+                    top = self.operatorsStack.pop()
+                    while top != '(':
+                        self.output.append(top)
+                        top = self.operatorsStack.pop()        
+                else:
+                    while (not self.operatorsStack.isEmpty()) and (self.greaterPrecedency(char)):
+                        self.output.append(self.operatorsStack.pop())
+                    self.operatorsStack.push(char)
+
+            while not self.operatorsStack.isEmpty():
+                self.output.append(self.operatorsStack.pop())
+
             
-            if ((char.isascii() and char not in ['+', '.', '*', '(', ')', '\\']) or flag): 
-                if flag:
-                    flag = False
-                    self.output.append('\\'+char)
-                else:    
-                    self.output.append(char)
-            elif (char == '\\'):
-                flag = True
+            no_space = ''.join(self.output)
+            no_space = no_space.split(' ')
+            no_space = ''.join(no_space)
+            self.output = list(no_space)
+            print(''.join(self.output))
+            
+        except:
+            self.output = []
+            print('expressão inválida')
 
-            elif char == '(':
-                self.operatorsStack.push(char)
-            elif char == ')':
-                top = self.operatorsStack.pop()
-                while top != '(':
-                    self.output.append(top)
-                    top = self.operatorsStack.pop()        
-            else:
-                while (not self.operatorsStack.isEmpty()) and (self.greaterPrecedency(char)):
-                    self.output.append(self.operatorsStack.pop())
-                self.operatorsStack.push(char)
+    def execute(self):
+        if self.output == []:
+            return print('expressão ainda não convertida') 
+        stack = Stack()    
+        x = 0
+        while(x <= (len(self.output)-1)):
+            
+            char = self.output[x]
+            if(char.isascii() and char not in ['+', '.', '*', '(', ')']):
+                if(char == '\\'):
+                    stack.push(char+input[x+1])
+                    x+=1
+                else:
+                    stack.push(char)
+            else: 
+                if(not stack.isEmpty()):
+                    operand2 = stack.pop()
+                    if(char == '*'):
+                        value = operand2
+                        stack.push(value) 
+                    else: 
+                        if(not stack.isEmpty()):
+                            operand1 = stack.pop()
+                            value = operand1+operand2
+                            stack.push(value)      
+                        else:
+                            return print('expressão inválida')        
+                else:
+                    return print('expressão inválida')
+            x+=1
+        operand1 = stack.pop()
 
-        while not self.operatorsStack.isEmpty():
-            self.output.append(self.operatorsStack.pop())
-
-        return self.output
+        if(stack.isEmpty()):
+            print('expressão válida')
+        else:
+            print('expressão inválida')
